2fbf80d0fc579e669bf924a8d2f5ed96676a36d8
Harsh <53746749+Harshsingh22@users.noreply.github.com>
2019-09-14 23:13:11 +0530

    Add files via upload

---
+++ multiple_linear_regression.py
@@ -0,0 +1,61 @@
+# Multiple Linear Regression
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('50_Startups.csv')
+X = dataset.iloc[:, :-1].values
+y = dataset.iloc[:, 4].values
+
+# Encoding categorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelencoder = LabelEncoder()
+X[:, 3] = labelencoder.fit_transform(X[:, 3])
+
+onehotencoder = OneHotEncoder(categorical_features = [3])
+X = onehotencoder.fit_transform(X).toarray()
+
+# Avoiding the Dummy Variable Trap
+X = X[:, 1:]
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting Multiple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set results
+y_pred = regressor.predict(X_test)
+y_pred_1 = regressor.predict(X_train)
+
+def mean_absolute_error(a,b):
+    a,b=np.array(a),np.array(b)
+    c=np.mean(np.abs((a-b/a)))
+    return c
+print(mean_absolute_error(y_test,y_pred,np.abs((a-b/a)))
+print(mean_absolute_error(y_train,y_pred_1))
+
+
+
+'''assignment-	check percentile-remove errors
+	scatterplot
+	reduce the error by checking the different patterns
+	feature engineering- create new X variables
+    interaction of variables
+    dropping variables
+    % wise outlier removal
+'''
